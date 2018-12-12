6f1e9a325d762044a495cb940e99547d1a358b1f
Mthabisi Ndlovu <mthabisi.cody.ndlovu@gmail.com>
2018-12-12 05:27:37 +0200

    Reset raggnet-frontend

--- raggnet-frontend/app/models/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 3001,
-
-    // location of backend, do not include trailing /
-    apiUrl: 'http://localhost:3000',
-
-    // secret for creating tokens
-    secret: 'reughdjfbxmdgvhbgakbhjsadf',
-};
