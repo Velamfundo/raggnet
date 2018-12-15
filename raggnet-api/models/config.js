3614a753f7b950efe0565cbebb181bde03840748
Mthabisi Ndlovu <mthabisi.cody.ndlovu@gmail.com>
2018-12-15 13:18:08 +0200

    Added db name to dbUrl

--- raggnet-api/models/config.js
+++ raggnet-api/models/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 3000,
-  dbUrl: 'mongodb://localhost:5000',
+  dbUrl: 'mongodb://localhost:5000/raggnet',
 
   // secret for creating tokens
   secret: 'reughdjfbxmdgvhbgakbhjsadf',
